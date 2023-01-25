c995070159a29e1d5ea31549ee59dd4df181f7be
Anastatia Dorval <ajdorval@knox.edu>
2023-01-25 16:25:26 -0600

    made a menu and can navigate pages

---
+++ js/measure.js
@@ -0,0 +1,10 @@
+document.getElementById("size").addEventListener("click", size);
+document.getElementById("popup").addEventListener("click", home);
+
+function size () {
+    window.location.href = "size.html"
+}
+
+function home () {
+    window.location.href = "popup.html"
+}
